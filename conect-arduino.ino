ef729aeb39205434fb00173be71dc663b222fa2e
Lounge Lizard dev <86478645+Lounge-Lizard-dev@users.noreply.github.com>
2021-09-28 13:56:20 +0200

    Add files via upload

---
+++ conect-arduino.ino
@@ -0,0 +1,30 @@
+const int pinX = A0;
+const int pinY = A1;
+const int pinBouton = 9;
+ 
+void setup() {
+   pinMode(pinBouton , INPUT_PULLUP);   
+   Serial.begin(9600);
+}
+ 
+void loop() {
+   int X = 0;
+   int Y = 0;
+   bool boutonValeur = false;
+ 
+ 
+   X = analogRead(pinX);
+  
+   delay(100);
+   Y = analogRead(pinY);
+   boutonValeur = digitalRead(pinBouton);
+ 
+ 
+   Serial.print("X:" );
+   Serial.print(X);
+   Serial.print(" | Y: ");
+   Serial.print(Y);
+   Serial.print(" | Pulsateur: ");
+   Serial.println(boutonValeur);
+   delay(1000);
+}
